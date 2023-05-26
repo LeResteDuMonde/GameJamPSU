371a35ea5a1154402106f2d4e99ff3705dc50bed
Basile Pesin <basile.pesin@vertmo.org>
2023-05-26 21:37:43 +0200

    fixed cursor

--- script/cursor.gd
+++ script/cursor.gd
@@ -6,21 +6,13 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _process(delta):
-	var mouse_rel = Vector2.ZERO
-	if Input.is_action_pressed("up"):
-		mouse_rel += Vector2.UP * mouse_speed
-	elif Input.is_action_pressed("down"):
-		mouse_rel += Vector2.DOWN * mouse_speed
-	elif Input.is_action_pressed("left"):
-		mouse_rel += Vector2.LEFT * mouse_speed
-	elif Input.is_action_pressed("right"):
-		mouse_rel += Vector2.RIGHT * mouse_speed
-	if mouse_rel != Vector2.ZERO:
-		Input.warp_mouse_position(position + mouse_rel)
-	this.position = get_global_mouse_position()
+	var mouse_rel = Input.get_vector("left", "right", "up", "down")
+	position = position + mouse_rel * mouse_speed
+#	this.position = get_global_mouse_position()
 
 func _input(event):
 	if Input.is_action_just_released("click"):
-		selecting = false
-	if event is InputEventMouseMotion:
-		mouse_pos = event.position
+		print("click")
+#		selecting = false
+#	if event is InputEventMouseMotion:
+#		position = event.position
