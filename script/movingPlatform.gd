1cda44755376caf8d61208c19bcece3ecfaa125a
samlekebab <voddfclj@duck.com>
2023-05-27 07:37:28 +0200

    angle moving platfom

--- script/movingPlatform.gd
+++ script/movingPlatform.gd
@@ -4,13 +4,15 @@ var speed : float = 0.01
 var amplitude : float = 100
 var direction : float = 1
 var zero
+var angle = 0
 var deleteNumber = 0
 func _ready():
 	zero = global_position;
 func _physics_process(delta):
 	# var velocity = get_velocity()
-	var move = Vector2( direction * speed * amplitude,0)
-	var deltax = global_position.x - zero.x
+	var move = Vector2(cos(angle)* direction * speed * amplitude,sin(angle)*direction * speed * amplitude)
+	var deltax = sqrt((global_position.x - zero.x)**2 +  (global_position.y - zero.y)**2)
+
 	if deltax > amplitude:
 		direction = -1
 	if deltax < -amplitude: 
