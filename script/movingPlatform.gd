f3c2295b0f000225d7c14c8663b673aed8d0b508
samlekebab <voddfclj@duck.com>
2023-05-27 14:29:24 +0200

    cruched by moving walls

--- script/movingPlatform.gd
+++ script/movingPlatform.gd
@@ -11,7 +11,7 @@ func _ready():
 	zero = global_position;
 func _physics_process(delta):
 	# var velocity = get_velocity()
-	var move = Vector2(cos(angle)* direction * speed * amplitude,sin(angle)*direction * speed * amplitude)
+	var move = Vector2(cos(angle)* direction * speed * amplitude * delta / 0.016,sin(angle)*direction * speed * amplitude * delta / 0.016)
 	var deltax = cos(angle)*(global_position.x - zero.x) +  sin(angle)*(global_position.y - zero.y)
 	#print_debug(move,deltax)
 	if deltax > amplitude:
@@ -19,17 +19,36 @@ func _physics_process(delta):
 	if deltax < -amplitude: 
 		direction = 1
 		
-	var collision = move_and_collide(move)
-	if collision:
-		var collider = collision.get_collider()
+	var collision = move_and_collide(move,true)
+	var collider
+	if collision :
+		collider = collision.get_collider()
 		var collName = collider.name
 		if collName == "StarShip":
 			pass
 		elif collName == "Player":
-			collider.move_and_collide(move)
-			move_and_collide(move)
+			var norm = collision.get_normal()
+			#print_debug(move.length())
+			var collision2 = collider.move_and_collide(move.dot(norm)*norm,true)
+			#print_debug(move.dot(norm)*norm)
+			#print_debug(collision.get_normal().dot(move))
+			if collision2:              #and abs(collision.get_normal().dot(norm))>0.08:
+				print_debug(collision2.get_collider().name)
+				GameManager.killPlayer()
+			#else:
+			
+			#print(collider.velocity)
+			collider.velocity = collider.velocity-collider.velocity.dot(norm)*norm
+			#print(collider.velocity)
+			#print(2*move.dot(norm)*norm)
+			collision2 = collider.move_and_collide(move.dot(norm)*norm,false)
+			add_collision_exception_with(collider)
 		else:
-			direction = -direction	
+			direction = -direction
+			
+	#print(move_and_collide(move))
+	if collision : 
+		remove_collision_exception_with(collider)
 		
 	
 func highlight(g):
