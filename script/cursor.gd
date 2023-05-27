0d9b163548165179092be83d7ce4f3aa50bc609e
Jules T460 <jules.pesin@gmail.com>
2023-05-27 03:37:55 +0200

    cursor debug

--- script/cursor.gd
+++ script/cursor.gd
@@ -5,7 +5,7 @@ var mouse_speed = 5.0
 func _process(delta):
 	var mouse_rel = Input.get_vector("left", "right", "up", "down")
 	position = position + mouse_rel * mouse_speed
-	print(position.x, ";", position.y)
+#	print(position.x, ";", position.y)
 #	this.position = get_global_mouse_position()
 
 func _input(event):
