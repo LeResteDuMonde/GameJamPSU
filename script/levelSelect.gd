1b3a2224d85bf00f39aa3f104c55b829521f70ef
Basile Pesin <basile.pesin@vertmo.org>
2023-05-27 18:33:23 +0200

    Level3

--- script/levelSelect.gd
+++ script/levelSelect.gd
@@ -19,8 +19,9 @@ func onAreaExited(area):
 		highlight.visible = false
 
 var level1 = preload("res://scene/Level1.tscn")
-
 var level2 = preload("res://scene/Level2.tscn")
+var level3 = preload("res://scene/Level3.tscn")
+
 func loadLevel(level,levelID):
 	GameManager.startGame(level.instantiate(),levelID)
 
@@ -29,3 +30,4 @@ func _input(event):
 	if event.is_action_pressed("click") && selected && TitleManager.levelSelectScreen.visible:
 		if name == "Level1": loadLevel(level1,1)
 		elif name == "Level2": loadLevel(level2,2)
+		elif name == "Level3": loadLevel(level3,3)
